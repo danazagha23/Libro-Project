b9b033f5c0dcf2c2f93b63c092b71301198e5e46
Dana Zagha <dana.zagha0@gmail.com>
2023-06-21 23:28:40 +0300

    Infrastructure layer changes

--- Libro.Infrastructure/Data/Migrations/20230621103430_SeedData2.Designer.cs
+++ Libro.Infrastructure/Data/Migrations/20230621103430_SeedData2.Designer.cs
@@ -4,6 +4,7 @@ using Libro.Infrastructure.Data.DbContexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Libro.Infrastructure.Migrations
 {
     [DbContext(typeof(LibroDbContext))]
-    partial class LibroDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230621103430_SeedData2")]
+    partial class SeedData2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
