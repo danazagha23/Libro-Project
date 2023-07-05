ece9b38c538cc8cfb9016be4e8a80d1be615414b
Dana Zagha <dana.zagha0@gmail.com>
2023-07-05 11:17:12 +0300

    Set up welcome page for unauthenticated users and patrons, and added dashboards for librarians and administrators

---
+++ Libro.Presentation/Models/HomeViewModel.cs
@@ -0,0 +1,9 @@
+﻿using Libro.Application.DTOs;
+
+namespace Libro.Presentation.Models
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<BookDTO> AvailableBooks { get; set; }
+    }
+}
